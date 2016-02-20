896910b3dc053eb7fb8ea912df91837b33c698c3
Michał Przyszczypkowski <michal@unit9.com>
2016-02-20 17:49:36 +0100

    copy levels for skill badges

---
+++ app/src/app/user/dashboard/skillValueBadge/badgeConfig.js
@@ -0,0 +1,14 @@
+export const badgeConfig = Object.freeze({
+  low: {
+    background: 'red',
+    copy: 'Not good enough. You need to work harder.'
+  },
+  medium: {
+    background: 'yellow',
+    copy: 'Quite good. Keep doing your job.'
+  },
+  high: {
+    background: 'lightgreen',
+    copy: 'Excellent. Almost perfect.'
+  }
+});
