b3348c472d638ff9e3a80b396c4e08d9b0695cfa
Patryk Ziemkowski <patryk.ziem@gmail.com>
2016-02-20 11:28:22 +0100

    Import lodash in index

--- app/src/main.js
+++ app/src/main.js
@@ -8,13 +8,15 @@ window.ngInject = (fn) => fn;
  */
 window.jQuery = window.$ = require('jquery');
 
+require('lodash');
+
 /**
  * Make sure angular is required before other node modules,
  * which often have imports with side effects
  */
 require('angular');
-
 /**
  * Include your application
  */
 require('./app');
+
