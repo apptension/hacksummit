1ca96ce65d651105ccc04580ebc53d778aa097b4
Daniel Capeletti <daniel.capeletti@unit9.com>
2016-02-20 23:22:30 +0100

    Accordion to user section

--- app/src/app/admin/settings/users/users.controller.js
+++ app/src/app/admin/settings/users/users.controller.js
@@ -1,3 +1,22 @@
-export default ngInject(function UsersController() {
+export default ngInject(function UsersController($scope, User) {
+  $scope.users = [];
+  User.getList().then((data) => {
+    $scope.users = data.map((user) => {
+      user.edit = false;
+      return user;
+    });
+  });
 
+  $scope.toggleItem = (selected) => {
+    if (selected.edit) {
+      selected.edit = false;
+      return;
+    }
+
+    $scope.users.forEach((user) => {
+      user.edit = false;
+    });
+
+    selected.edit = true;
+  };
 });
