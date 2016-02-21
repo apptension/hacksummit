b76b0214507d1616162b6c8ef7117033357bb96a
Wojciech Panek <wpanek@apptension.com>
2016-02-21 13:16:25 +0100

    add projects dialog

---
+++ app/src/app/admin/settings/projects/components/projectForm/projectFrom.js
@@ -0,0 +1,43 @@
+import template from './projectForm.html';
+
+
+export default ngInject(() => {
+  return {
+    restrict: 'AE',
+    template: template,
+    link: function ($scope, element, attr) {
+
+      $scope.getUsersCollection = () => {
+        let modelAttribute = attr.projectFormUsers.replace(/\[(\w+)\]/g, '.$1');
+        modelAttribute = modelAttribute.replace(/^\./, '');
+        let modelAttributeArray = modelAttribute.split('.');
+        let modelObject = $scope;
+        modelAttributeArray.forEach((property) => {
+          if (!_.isUndefined(modelObject[property])) {
+            modelObject = modelObject[property]
+          }
+        });
+        return modelObject;
+      };
+
+      $scope.getFormModel = () => {
+        let modelAttribute = attr.projectFormModel.replace(/\[(\w+)\]/g, '.$1');
+        modelAttribute = modelAttribute.replace(/^\./, '');
+        let modelAttributeArray = modelAttribute.split('.');
+        let modelObject = $scope;
+        modelAttributeArray.forEach((property) => {
+          if (!_.isUndefined(modelObject[property])) {
+            modelObject = modelObject[property]
+          }
+        });
+        return modelObject;
+      };
+
+      $scope.searchUser = (input, excluded) => {
+        return $scope.getUsersCollection().filter((user) => {
+            return user.name.toLowerCase().indexOf(angular.lowercase(input)) >= 0 && excluded.indexOf(user) === -1;
+          }) || [];
+      };
+    }
+  };
+});
--- app/src/app/admin/settings/projects/index.js
+++ app/src/app/admin/settings/projects/index.js
@@ -5,10 +5,12 @@ import routing from './projects.routes.js';
 
 import projectsController from './projects.controller.js';
 
+import projectFormDirective from './components/projectForm/projectFrom'
 
 export default angular.module('app.admin.projects', [
   uirouter
 ])
   .controller('ProjectsController', projectsController)
+  .directive('projectForm', projectFormDirective)
   .config(routing)
   .name;
