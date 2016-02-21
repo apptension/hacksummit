4ca51adf6b806da0c98ea44cb969e77c561bae42
Patryk Ziemkowski <patryk.ziem@gmail.com>
2016-02-21 19:31:42 +0100

    Remove ugly $timeout from userStatsChart directive

--- app/src/app/user/dashboard/userStatsChart.directive.js
+++ app/src/app/user/dashboard/userStatsChart.directive.js
@@ -9,71 +9,69 @@ export default ngInject(($window, $timeout) => {
       skillpointSelected: '&'
     },
     link: (scope, element) => {
-      $timeout(() => {
-        let width = element.width();
-        let height = element.height();
-        let window = angular.element($window);
-        let lineChart = LineChart({width, height}); // eslint-disable-line new-cap
+      let width = element.width();
+      let height = element.height();
+      let window = angular.element($window);
+      let lineChart = LineChart({width, height}); // eslint-disable-line new-cap
 
-        lineChart.on('valueSelected', dispatchSelectedValue);
-        window.on('resize', adjustChartSize);
-        scope.$watch('data', render, true);
-        scope.$on('$destroy', () => {
-          render([]);
-          window.off('resize', adjustChartSize);
-        });
-
-        function dispatchSelectedValue(d) {
-          scope.$apply(() => {
-            scope.skillpointSelected({date: d});
-          });
-        }
-
-        function adjustChartSize() {
-          width = element.width();
-          height = element.height();
-
-          lineChart.width(width).height(height);
-          render(scope.data);
-        }
-
-        function render(data) {
-          if (!data) {
-            return;
-          }
-
-          let svg = d3.select(element[0]).selectAll('svg')
-            .data([{
-              series: _.map(data, (skill) => {
-                return {
-                  name: skill.name,
-                  color: skill.color,
-                  isDashed: skill.isDashed,
-                  values: _.map(skill.scores, (score) => {
-                    return {
-                      x: score.date,
-                      y: score.value,
-                      color: skill.color,
-                      commentsCount: _.reduce(skill.comments, (sum, comment) => {
-                        return sum + (comment.date.isSame(score.date) ? 1 : 0);
-                      }, 0)
-                    };
-                  })
-                };
-              })
-            }]);
-
-          svg.enter()
-            .append('svg');
-
-          svg.attr({
-            width: width,
-            height: height,
-            viewBox: `0 0 ${width} ${height}`
-          }).call(lineChart);
-          svg.exit().remove();
-        }
+      lineChart.on('valueSelected', dispatchSelectedValue);
+      window.on('resize', adjustChartSize);
+      scope.$watch('data', render, true);
+      scope.$on('$destroy', () => {
+        render([]);
+        window.off('resize', adjustChartSize);
       });
+
+      function dispatchSelectedValue(d) {
+        scope.$apply(() => {
+          scope.skillpointSelected({date: d});
+        });
+      }
+
+      function adjustChartSize() {
+        width = element.width();
+        height = element.height();
+
+        lineChart.width(width).height(height);
+        render(scope.data);
+      }
+
+      function render(data) {
+        if (!data) {
+          return;
+        }
+
+        let svg = d3.select(element[0]).selectAll('svg')
+          .data([{
+            series: _.map(data, (skill) => {
+              return {
+                name: skill.name,
+                color: skill.color,
+                isDashed: skill.isDashed,
+                values: _.map(skill.scores, (score) => {
+                  return {
+                    x: score.date,
+                    y: score.value,
+                    color: skill.color,
+                    commentsCount: _.reduce(skill.comments, (sum, comment) => {
+                      return sum + (comment.date.isSame(score.date) ? 1 : 0);
+                    }, 0)
+                  };
+                })
+              };
+            })
+          }]);
+
+        svg.enter()
+          .append('svg');
+
+        svg.attr({
+          width: width,
+          height: height,
+          viewBox: `0 0 ${width} ${height}`
+        }).call(lineChart);
+        svg.exit().remove();
+      }
     }
   };
 });
