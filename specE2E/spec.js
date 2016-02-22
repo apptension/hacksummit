eaf0cbea4dd1db6a665f23be7d03be038e44ff67
tymoteuszb <tymoteusz.bleja@outlook.com>
2016-02-22 06:33:14 +0100

    removed test

--- specE2E/spec.js
+++
@@ -1,9 +0,0 @@
-describe('Protractor demo', () => {
-  beforeEach(() => {
-    browser.get('/');
-  });
-
-  it('should loade page', () => {
-    expect(browser.getTitle()).toEqual('Title');
-  });
-});
