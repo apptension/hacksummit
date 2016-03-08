f7cb807be31b8e342352a85e41fb5080e679df9d
Wojciech Panek <wpanek@apptension.com>
2016-03-08 15:32:02 +0100

    remove unused requests

--- app/src/app/app.controller.js
+++ app/src/app/app.controller.js
@@ -1,4 +1,3 @@
 export default ngInject(function AppController(User) {
-  User.getList().then(() => {}
-  );
+
 });
--- app/src/app/home/home.controller.js
+++ app/src/app/home/home.controller.js
@@ -1,19 +1,5 @@
 import parallax from 'vendor_modules/parallax/jquery.parallax.js';
 
 export default ngInject(function HomeController($scope, $state, User) {
-  User.getList().then((data) => {
-    let users = data.plain();
-    this.admin = _.find(users, 'isAdmin');
-    this.users = _.without(users, this.admin);
-  });
 
-  this.startAs = (userId) => {
-    User.login(userId).then(() => {
-      if (userId === this.admin.id) {
-        $state.go('app.admin.home');
-      } else {
-        $state.go('app.user.dashboard');
-      }
-    });
-  };
 });
